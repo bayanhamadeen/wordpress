8d13d5044b32f79bd5e2c89a10864732f1ff5ec6
bayanhamadeen <61909359+bayanhamadeen@users.noreply.github.com>
2022-08-22 00:38:15 +0300

    Update and rename bayan to bootstrap-wordpress.sh

--- bootstrap-wordpress.sh
+++ bootstrap-wordpress.sh
@@ -1,17 +1,52 @@
-sudo yum update -y
-sudo amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
-sudo yum install -y httpd mariadb-server
-sudo systemctl start httpd
-sudo systemctl enable httpd
-sudo usermod -a -G apache ec2-user
-groups
-sudo chown -R ec2-user:apache /var/www
-sudo chmod 2775 /var/www && find /var/www -type d -exec sudo chmod 2775 {} \;
-find /var/www -type f -exec sudo chmod 0664 {} \;
-echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
-wget https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz
-cp wordpress/wp-config-sample.php wordpress/wp-config.php
+#!/bin/bash -xe
+
+cd /tmp
+
+# Install jq
+yum -y install jq
+
+# Populate some variables from meta-data and tags
+INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl http://169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+NAME=$(aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=Name" "Name=resource-id,Values=$INSTANCE_ID" --output json | jq .Tags[0].Value -r)
+STACK_NAME=$(aws ec2 describe-tags --region us-east-1 --filters "Name=key,Values=StackName" "Name=resource-id,Values=$INSTANCE_ID" --output json | jq .Tags[0].Value -r)
+
+
+
+
+
+################################################################################
+# BEGIN WordPress Setup
+################################################################################
+
+# TBD AWS Secrets Manager?
+DB_USER=$(awk -F "=" '/DB_USER/ {print $2}' /root/.aws/bootstrap.properties)
+DB_PASSWORD=$(awk -F "=" '/DB_PASSWORD/ {print $2}' /root/.aws/bootstrap.properties)
+DB_NAME=$(awk -F "=" '/DB_DATABASE/ {print $2}' /root/.aws/bootstrap.properties)
+DB_HOST=$(awk -F "=" '/DB_SERVER/ {print $2}' /root/.aws/bootstrap.properties)
+
+# Get the latest LAMP packages for AWS Linux 2
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+
+# Install apache
+yum install -y httpd
+
+# Install php-xml
+yum install -y php-xml.*
+
+# Remove this later
+echo "<?php phpinfo() ?>" > /var/www/html/info.php
+
+# Download wordpress and install it in /var/www/html
+cd /tmp
+wget -O /tmp/latest.tar.gz http://wordpress.org/latest.tar.gz
+tar -xzf /tmp/latest.tar.gz
+rm -rf /var/www/html/wp-*
+mv /tmp/wordpress/* /var/www/html
+chown -R apache:apache /var/www/html
+chmod 775 /var/www/html
+cd /var/www/html
+
 # Configure wordpress
 mv wp-config-sample.php wp-config.php 
 sed -i "s/define( *'DB_USER', '.*' *);/define( 'DB_USER', '${DB_USER}' );/" wp-config.php 
@@ -20,7 +55,9 @@ sed -i "s/define( *'DB_NAME', '.*' *);/define( 'DB_NAME', '${DB_NAME}' );/" wp-c
 sed -i "s/define( *'DB_HOST', '.*' *);/define( 'DB_HOST', '${DB_HOST}' );/" wp-config.php 
 
 
-##
+# Lock down wp-config.php
+chmod 660 wp-config.php
+
 # Configure cache expiry for static content
 cat << EOF > /var/www/html/.htaccess
 # BEGIN WordPress
@@ -91,3 +128,6 @@ rm -rf /home/ec2-user || true
 # Call cfn-init, which reads the launch configration metadata and uses it to
 # configure and runs cfn-hup as a service, so we can get a script run on updates to the metadata
 /opt/aws/bin/cfn-init -v --stack ${STACK_NAME} --resource LaunchTemplate --configsets cfn_install --region ${REGION}
+
+# Send a signal indicating we're done
+/opt/aws/bin/cfn-signal -e $? --stack ${STACK_NAME} --resource WordPressScalingGroup --region ${REGION} || true
